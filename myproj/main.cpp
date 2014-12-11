28b39b2aab647ff3dd702d13cbfb904e9014f76e
rubeus <rubeus90@gmail.com>
2014-12-11 16:42:28 +0100

    Read 6 images and stock them in a texture

--- myproj/main.cpp
+++ myproj/main.cpp
@@ -274,6 +274,83 @@ void display()
 	glFlush();
 }
 
+GLubyte* readTexture(char* filename){
+	FILE *inFile;
+	char buffer[100];
+	GLubyte *mytexture;
+	unsigned char c;
+	int maxVal, width, height, pixelsize;
+
+	if ((inFile = fopen(filename, "rb")) == NULL) {
+		return 0;
+	}
+
+	//Read file type identifier (magic number)
+	fgets(buffer, sizeof(buffer), inFile);
+	if ((buffer[0] != 'P') || (buffer[1] != '6')) {
+		fprintf(stderr, "not a binary ppm file %s\n", filename);
+		return 0;
+	}
+
+	if (buffer[2] == 'A')
+		pixelsize = 4;
+	else
+		pixelsize = 3;
+
+	//Read image size
+	do fgets(buffer, sizeof(buffer), inFile);
+	while (buffer[0] == '#');
+	sscanf(buffer, "%d %d", &width, &height);
+
+	//Read maximum pixel value (usually 255)
+	do fgets(buffer, sizeof(buffer), inFile);
+	while (buffer[0] == '#');
+	sscanf(buffer, "%d", &maxVal);
+
+	//Allocate RGBA texture buffer
+	int memSize = width * height * 4 * sizeof(GLubyte);
+	mytexture = new GLubyte[memSize];
+
+	// read RGB data and set alpha value
+	for (int i = 0; i < memSize; i++) {
+		if ((i % 4) < 3 || pixelsize == 4) {
+			c = fgetc(inFile);
+			mytexture[i] = (GLubyte)c;
+		}
+		else mytexture[i] = (GLubyte)255; //Set alpha to opaque
+	}
+	fclose(inFile);
+
+	return mytexture;
+}
+
+void cube_mapping(){
+	GLuint cubemap_texture;
+	GLubyte** cubeMap;
+	cubeMap[0] = readTexture("1.ppm");
+	cubeMap[1] = readTexture("2.ppm");
+	cubeMap[2] = readTexture("3.ppm");
+	cubeMap[3] = readTexture("4.ppm");
+	cubeMap[4] = readTexture("5.ppm");
+	cubeMap[5] = readTexture("6.ppm");
+
+	glEnable(GL_TEXTURE_CUBE_MAP);
+	glGenTextures(1, &cubemap_texture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap_texture);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER,
+		GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER,
+		GL_LINEAR);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	for (int i = 0; i<6; i++) {
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, 256, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, cubeMap[i]);
+	}
+}
+
 //This function is called from the main to initalize everything.
 void init()
 {
